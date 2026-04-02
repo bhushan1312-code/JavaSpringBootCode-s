576b01ca8be4f46314d57a7057a42a0068ccc5bc
Bhushan Avhad <bhushanavhad1312@gmail.com>
2026-04-02 21:58:11 +0530

    xmlwayconfiguration

---
+++ springxmlwayconfiguration/src/main/java/org/example/App.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("spring.cfg.xml");
+
+        product   product =  context.getBean("product", product.class);
+
+        System.out.println(product);
+    }
+}
